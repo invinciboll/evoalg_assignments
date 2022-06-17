51bbe77b8c9f539ccc2f8d0ca3d854c499048e25
invinciboll <sebastian.boll@uni-ulm.de>
2022-06-17 18:49:05 +0200

    Add some stuff of selection

--- Assignment_06/ex1.R
+++ Assignment_06/ex1.R
@@ -32,32 +32,87 @@ evaluate_individual <- function(individual) {
   }
 }
 
-# GA
-# GA <- function(population_size, generations, op_recombination, recombine_prob, op_mutation, mutate_prob, selection_pressure) {
+# # SELECTION
+sus_selector <- function(r, delta, fitness_matrix) {
+  for (i in rev(seq_len(nrow(fitness_matrix)))) {
+    if (r > fitness_matrix[i,4]){
+      return(i+1)
+    }
+  }
+  return(0)
+}
 
-GA <- function(population_size) {
+sus <- function(fitness_matrix) {
+    offspring <- list()
+    # erstellen des fitness proportion vektors
+    sum_fitness <- sum(fitness_matrix[, 2])
+    proportions <- round(sum_fitness / fitness_matrix[, 2], 2)
+    fitness_matrix <- cbind(fitness_matrix, proportions / 100)
+
+    # Add column for lower limits of regions
+    tmp <- 0
+    upper_boarders <- c()
+    for(i in seq_len(nrow(fitness_matrix))) {
+      tmp <- tmp + fitness_matrix[i, 3]
+      upper_boarders <- c(upper_boarders, tmp)
+    }
+    fitness_matrix <- cbind(fitness_matrix, upper_boarders)
+    
+    r <- round(runif(1), 4)
+
+    # Debug:
+    print(fitness_matrix)
+    print("r:")
+    print(r)
+    print("selected:")
+    print(sus_selector(r, 1, fitness_matrix))
+
+    # TODO: den selector in einer schleife aufrufen um genug individuen ("IDs") zu bekommen
+}
+
+
+# # GA
+GA <- function(population_size, max_generations) {
   # INITIALISE
   population <- init(population_size)
-
+  
   # EVALUATE
   fitness_matrix <- matrix(nrow=population_size, ncol=2)
-
   for (i in seq_len(population_size)) {
-    fitness_matrix[i, ] <- c(i, evaluate_individual(population[[i]]))
+    fitness <- evaluate_individual(population[[i]])
+    fitness_matrix[i, ] <- c(i, fitness)
+    if (fitness == 0) {
+      # If one offspring has fitness == 0 we can stop right here, but that would be very lucky
+      solved <- TRUE
+    }
   }
-  print(fitness_matrix)
 
-  # REPEAT
-  # for (i in seq_len(generations)){
+  # REPEAT (until solving permutation is found or max generations are exceeded)
+  current_generation <- 0
+  solved <- FALSE
+  while(!solved && (current_generation < max_generations)) {
     # SELECT
-    
+    sus(fitness_matrix)
     # RECOMBINE
+    offspring <- list()
     # MUTATE
-    # EVALUATE offspring
-    # SELECT survivors (or replacement): replace the whole population with the resulting offspring (and random choices to reach the population size)
+    
 
-  # }
-  # Return something
+    # EVALUATE offspring
+    for (i in seq_len(population_size)) {
+      fitness <- evaluate_individual(offspring[[i]])
+      fitness_matrix[i, ] <- c(i, fitness)
+      if (fitness == 0) {
+        # If one offspring has fitness == 0 we can terminate the GA
+        solved <- TRUE
+      }
+    }
+    # SELECT survivors (or replacement): replace the whole population with the resulting offspring (and random choices to reach the population size)
+    
+    
+    current_generation <- current_generation + 1
+  }
+  # Return the solving permutation
 }
 
 
@@ -68,4 +123,4 @@ GA <- function(population_size) {
 
 set.seed(as.numeric(Sys.time()))
 
-GA(10)
+GA(population = 10, max_generations = 100)
