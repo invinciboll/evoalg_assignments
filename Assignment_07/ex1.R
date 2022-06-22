2024067e8eb1d914737d77b3f03e09e52fbd8dd5
invinciboll <sebastian.boll@uni-ulm.de>
2022-06-22 17:36:05 +0200

    Fix selection and recomb to match requirements

--- Assignment_07/ex1.R
+++ Assignment_07/ex1.R
@@ -31,12 +31,14 @@ recombine <- function(population) {
     offspring <- matrix(, nrow = nrow(population), ncol = 3)
     colnames(offspring) <- c("x", "y", "fitness")
     for (i in seq_len(nrow(population))) {
-        # Parents get selected without fitness bias
-        # Get 2 random chosen parents (its possible to chose the same individual for both parents)
-        parent_indexes <- floor(runif(2, 1, nrow(population) + 1))
+        # Get 1 random chosen partner (exclude the individual itself)
+        partner_index <- i
+        while (partner_index == i) {
+            partner_index <- floor(runif(1, 1, nrow(population) + 1))
+        }
         # Intermediary recombine them
-        offspring[i, 1] <- mean(c(population[parent_indexes[1], 1], population[parent_indexes[2], 1]), trim = 0)
-        offspring[i, 2] <- mean(c(population[parent_indexes[1], 2], population[parent_indexes[2], 2]), trim = 0)
+        offspring[i, 1] <- mean(c(population[i, 1], population[partner_index, 1]), trim = 0)
+        offspring[i, 2] <- mean(c(population[i, 2], population[partner_index, 2]), trim = 0)
         offspring[i, 3] <- NA
     }
     return(offspring)
@@ -44,16 +46,26 @@ recombine <- function(population) {
 
 # Select survivors by 5+5 strategy
 select_survivors <- function(population, offspring) {
-    print(population)
-    print(offspring)
-    all <- rbind(population, offspring)
-    all <- all[order(all[, 3]), ]
-    print(all)
-    print(all[(nrow(all) / 2 + 1):nrow(all), ])
+    stopifnot(nrow(population) == nrow(offspring))
+
+    # Compare parent with its offspring, keep the fitter one. Children are favoured in case of a draw.
+    new_population <- matrix(, nrow = nrow(population), ncol = ncol(population))
+    colnames(new_population) <- c("x", "y", "fitness")
+    for (i in seq_len(nrow(population))) {
+        if (population[i, 3] > offspring[i, 3]) {
+            new_population[i, ] <- population[i, ]
+        } else if (offspring[i, 3] >= population[i, 3]) {
+            new_population[i, ] <- offspring[i, ]
+        } else {
+            stop()
+        }
+    }
+    return(new_population)
 }
 
 ES <- function(population_size) {
     params <- load_rdata("params.RData")
+
     # INITIALIZE
     population <- init(population_size)
 
@@ -70,13 +82,17 @@ ES <- function(population_size) {
 
         # MUTATE
 
-        # EVALUATE
+        # EVALUATE offspring
         for (i in seq_len(nrow(offspring))) {
             offspring[i, 3] <- fitness(offspring[i, 1], offspring[i, 2], params)
         }
 
         # SELECT
+        print(population)
+        print(offspring)
+
         population <- select_survivors(population, offspring)
+        print(population)
         terminate <- TRUE
     }
 }
