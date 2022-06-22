1102c33f5633efe0277eba81fb4fc8c888547373
invinciboll <sebastian.boll@uni-ulm.de>
2022-06-22 18:49:27 +0200

    Add adapting mutation (draft)

--- Assignment_07/ex1.R
+++ Assignment_07/ex1.R
@@ -11,7 +11,7 @@ init <- function(population_size) {
     population <- matrix(, nrow = population_size, ncol = 5)
     colnames(population) <- c("x", "y", "sigma_x", "sigma_y", "fitness")
     for (i in seq_len(nrow(population))) {
-        population[i, ] <- c(sample(-5:5, 2), NA, NA, NA)
+        population[i, ] <- c(sample(-5:5, 2), runif(2), NA)
     }
     return(population)
 }
@@ -39,16 +39,27 @@ recombine <- function(population) {
         # Intermediary recombine them
         offspring[i, 1] <- mean(c(population[i, 1], population[partner_index, 1]), trim = 0)
         offspring[i, 2] <- mean(c(population[i, 2], population[partner_index, 2]), trim = 0)
-        offspring[i, 3] <- NA
-        offspring[i, 4] <- NA
+        offspring[i, 3] <- mean(c(population[i, 3], population[partner_index, 3]), trim = 0)
+        offspring[i, 4] <- mean(c(population[i, 4], population[partner_index, 4]), trim = 0)
         offspring[i, 5] <- NA
     }
-    print(offspring)
+
     return(offspring)
 }
 
-mutate <- function() {
+mutate <- function(offspring) {
+    n <- 2 # 2 chromosomes (x, y) and 2 sigmas
+    # Mutate sigma
+    tau_a <- (1 / sqrt(2 * n))
+    tau_b <- (1 / (2 * sqrt(n)))
 
+    for (j in seq_len(nrow(offspring))) {
+        for (i in seq_len(n)) { # 2 sigmas
+            offspring[j, i + 2] <- offspring[j, i + 2] * exp(tau_a * runif(1, 0, 1) + tau_b * runif(1, 0, 1))
+            offspring[j, i] <- offspring[j, i] + offspring[j, i + 2] * runif(1, 0, 1)
+        }
+    }
+    return(offspring)
 }
 
 # Select survivors by 5+5 strategy
@@ -58,8 +69,7 @@ select_survivors <- function(population, offspring) {
     # Compare parent with its offspring, keep the fitter one. Children are favoured in case of a draw.
     new_population <- matrix(, nrow = nrow(population), ncol = ncol(population))
     colnames(new_population) <- c("x", "y", "sigma_x", "sigma_y", "fitness")
-    print(population)
-    print(offspring)
+
     for (i in seq_len(nrow(population))) {
         if (population[i, 5] > offspring[i, 5]) {
             new_population[i, ] <- population[i, ]
@@ -89,15 +99,16 @@ ES <- function(population_size) {
         # RECOMBINE
         offspring <- recombine(population)
 
+        print(offspring)
         # MUTATE
-
+        offspring <- mutate(offspring)
+        print(offspring)
         # EVALUATE offspring
         for (i in seq_len(nrow(offspring))) {
             offspring[i, 5] <- fitness(offspring[i, 1], offspring[i, 2], params)
         }
 
         # SELECT
-
         population <- select_survivors(population, offspring)
 
         terminate <- TRUE
