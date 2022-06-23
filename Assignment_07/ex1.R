d05ba1a97565dd9dcd57dc08ebcf054c56425de1
invinciboll <sebastian.boll@uni-ulm.de>
2022-06-24 01:08:55 +0200

    Add param gen, imrpove plots

--- Assignment_07/ex1.R
+++ Assignment_07/ex1.R
@@ -1,32 +1,55 @@
 # Josef Mayer, Sebastian Boll
 setwd("C:\\Users\\sboll\\studium\\EvoAlg\\Assignments\\assignment_07")
 library("mvtnorm")
-# # # Helper functions
+
+# Code Sections
+# 1. Helper functions
+# 2. ES functions and algorithm
+# 3. Run section given data
+# 4. Run section own data
+
+
+# # # 1. Helper functions
 load_rdata <- function(filename) {
     # Object needs to be named 'params'
     load(filename)
     return(params)
 }
 
-plot_gaussians <- function(params) {
+plot_gaussians <- function(params, population, first_run, z, current_generation) {
     x.points <- seq(-8, 8, length.out = 100)
     y.points <- x.points
-    z <- matrix(0, nrow = 100, ncol = 100)
 
-    for (k in seq_len(ncol(params))) {
-        mu <- c(params[1, k], params[2, k])
-        sigma <- matrix(c(params[3, k], 0, 0, params[4, k]), ncol = 2)
-        for (i in 1:100) {
-            for (j in 1:100) {
-                z[i, j] <- z[i, j] + dmvnorm(c(x.points[i], y.points[j]), mean = mu, sigma = sigma)
+    if (first_run) {
+        for (k in seq_len(ncol(params))) {
+            mu <- c(params[1, k], params[2, k])
+            sigma <- matrix(c(params[3, k], 0, 0, params[4, k]), ncol = 2)
+            for (i in 1:100) {
+                for (j in 1:100) {
+                    z[i, j] <- z[i, j] + params[5, k] * dmvnorm(c(x.points[i], y.points[j]), mean = mu, sigma = sigma)
+                }
             }
+            cat("\r\t\t\t\tPlotting gaussians:\t", floor(k / ncol(params) * 100), "%")
         }
-        cat("\rPlotting gaussians:\t", floor(k / ncol(params) * 100), "%")
     }
-    contour(x.points, y.points, z)
-}
-# # # ----------------
 
+
+    filled.contour(x.points, y.points, z, nlevels = 10, plot.axes = {
+        points(population[1, ], population[2, ], plot.title = title(paste("Generations: ", current_generation)))
+    })
+
+    return(z)
+}
+
+
+plot_fitness_evolution <- function(data, generations) {
+    plot(c(1:generations), data[, 1], plot.title = title("Fitness evolution"), xlab = "Generation", ylab = "Fitness", type = "o", col = "blue", pch = ".", lty = 1, ylim = range(min(data), max(data)))
+    for (i in seq_len(ncol(data))) {
+        lines(c(1:generations), data[, i], col = i, lty = 2)
+    }
+}
+
+# # # 2. ES functions
 init <- function(population_size) {
     population <- matrix(, nrow = population_size, ncol = 5)
     colnames(population) <- c("x", "y", "sig_x", "sig_y", "fitness")
@@ -67,6 +90,7 @@ recombine <- function(population) {
     return(offspring)
 }
 
+# Self adapting non-correlating mutation with a sigma for each chromosome
 mutate <- function(offspring) {
     n <- 2 # 2 chromosomes (x, y) and 2 sigmas
     # Mutate sigma
@@ -102,8 +126,10 @@ select_survivors <- function(population, offspring) {
     return(new_population)
 }
 
-ES <- function(population_size, generations) {
-    params <- load_rdata("params.RData")
+ES <- function(population_size, generations, filename, filename_plots) {
+    params <- load_rdata(filename)
+    fitness_evolution <- matrix(, nrow = 0, ncol = population_size)
+    cat("\tStarting...\n")
 
     # INITIALIZE
     population <- init(population_size)
@@ -114,8 +140,13 @@ ES <- function(population_size, generations) {
     }
 
     # REPEAT
-    current_generation <- 0
-    while (current_generation < generations) {
+    pdf(file = filename_plots)
+    current_generation <- 1
+
+    # Boolean to improve plotting speed
+    first_run <- TRUE
+
+    while (current_generation <= generations) {
         # RECOMBINE
         offspring <- recombine(population)
 
@@ -130,19 +161,47 @@ ES <- function(population_size, generations) {
         # SELECT
         population <- select_survivors(population, offspring)
 
+        cat("\r\tES Progress:\t", floor(current_generation / generations * 100), "%,")
+        # Generate first + 4 plots per ES run
+        if (((current_generation %% (generations / 4)) == 0) || first_run) {
+            z <- plot_gaussians(params, population, first_run, z, current_generation)
+            first_run <- FALSE
+        }
+
+        fitness_evolution <- rbind(fitness_evolution, c(population[5, ]))
         current_generation <- current_generation + 1
-        cat("\rES Progress:\t", floor(current_generation / generations * 100), "%")
     }
+    plot_fitness_evolution(fitness_evolution, generations)
+    # plot_sigma_evolution(fitness_evolution)
+    dev.off()
 
-    cat("\n")
-    print(population)
-    # sig <- matrix(c(1.430, 0, 0, 2.559), ncol = 2)
-    # print(sig)
-    # contour(dmvnorm(x = c(0, 0), mean = c(-1.062, 4.539)))
-    # # contour(params)
-    # print(params)
-
-    plot_gaussians(params)
+    cat("\n\tDone.")
 }
 
-ES(population_size = 5, generations = 100)
+
+# # # 3. Run with given params data
+cat("ES with given data:\n")
+z <- matrix(0, nrow = 100, ncol = 100)
+ES(population_size = 5, generations = 200, filename = "params.RData", filename_plots = "plots-given-data.pdf")
+
+# # 4. Run with given own data
+cat("\n\nES with own data:\n")
+own_data_filename <- "own_params.RData"
+if (!file.exists((own_data_filename))) {
+    cat("\tOwn data file not found. Generating new gaussian data...\n")
+
+    params <- data.frame(row.names = c("mn.x", "mn.y", "sd.x", "sd.y", "scale"), stringsAsFactors = default.stringsAsFactors())
+    for (i in seq_len(10)) {
+        data <- c(runif(2, min = -7, max = 7), runif(2, min = 1, max = 3), runif(1, min = 0.5, max = 2))
+        params$NewCen <- data
+        names(params)[names(params) == "NewCen"] <- gsub(" ", "", paste("Cen", i, collapse = NULL))
+    }
+    save(params, file = own_data_filename)
+    cat("\tGaussian data generated.\n")
+} else {
+    cat("\tOwn data file found!\n")
+}
+
+
+z <- matrix(0, nrow = 100, ncol = 100)
+ES(population_size = 5, generations = 200, filename = own_data_filename, filename_plots = "plots-own-data.pdf")
